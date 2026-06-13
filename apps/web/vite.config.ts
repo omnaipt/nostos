aaa2b92e77e694f15ee8814c888a28769c4af3ed
omnaipt <266693362+omnaipt@users.noreply.github.com>
2026-06-13 10:53:04 +0100

    chore: add vite config

---
+++ apps/web/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+export default defineConfig({
+  server: { host: "::", port: 8080 },
+  plugins: [react()],
+  resolve: {
+    dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime"],
+    alias: { "@": path.resolve(__dirname, "./src") },
+  },
+});
